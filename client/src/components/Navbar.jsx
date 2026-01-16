e32d9ee06b4f073cfd6f6f4020eb3831a7e4f4d0
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2026-01-16 07:30:43 +0600

    update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { Link, useNavigate } from 'react-router-dom';
+import { MenuIcon, XIcon } from 'lucide-react';
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = React.useState(false);
@@ -19,19 +20,18 @@ const Navbar = () => {
                     </div>
                          <div>
                         <button className='max-sm:hidden cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
-                        <svg onClick={() => setMenuOpen(true)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="sm:hidden"><path d="M4 5h16" /><path d="M4 12h16" /><path d="M4 19h16" /></svg>
+                       <MenuIcon onClick={() => setMenuOpen(true)} className='sm:hidden'/>
                     </div>
 
                 </div>
                 {/* Mobile Menu */}
                 <div className={`sm:hidden fixed inset-0 ${menuOpen ? 'w-full' : 'w-0'} overflow-hidden bg-white backdrop-blur shadow-xl rounded-lg z-[200] text-sm transition-all`}>
                     <div className='flex flex-col items-center justify-center h-full text-xl font-semibold gap-6 p-4'>
-                        <a href='#' onClick={() => scrollTo(0, 0)}> Home </a>
-                        <a href='#' onClick={() => scrollTo(0, 0)}> Products </a>
-                        <a href='#' onClick={() => scrollTo(0, 0)}> About </a>
-                        <a href='#' onClick={() => scrollTo(0, 0)}> Contact </a>
+                        <Link to='/marketplace' onClick={() => setMenuOpen(false)}> Marketplace </Link>
+                        <button> Messages </button>
+                        <button> My Listings </button>
                         <button className=' cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
-                        <svg onClick={() => setMenuOpen(false)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="absolute size-8 right-6 top-6 text-gray-500 hover:text-gray-700 cursor-pointer"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
+                        <XIcon onClick={() => setMenuOpen(false)} className='absolute size-8 right-6 top-6 text-gray-500 hover:text-gray-700 cursor-pointer'/>
                     </div>
                 </div>
             </nav>
