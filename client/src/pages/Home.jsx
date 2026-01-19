ec157102d7aa4924a10579672122895be02b151c
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2026-01-19 08:26:20 +0600

    Update Latest Listing Compunent use in data pass to react redux

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Hero from '../components/Hero'
+import React from "react";
+import Hero from "../components/Hero";
+import LatestListing from "../components/LatestListing";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <LatestListing />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
