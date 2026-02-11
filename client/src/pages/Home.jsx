487088f8753d833d6f51c9f7c65e7ba6f93f3bf1
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2026-02-12 01:17:28 +0600

    CAT & Footer Commpunent Ready

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Hero from "../components/Hero";
 import LatestListing from "../components/LatestListing";
 import Plans from "../components/Plans";
+import CTA from "../components/CTA";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero />
       <LatestListing />
       <Plans />
+      <CTA />
+      <Footer />
     </div>
   );
 };
