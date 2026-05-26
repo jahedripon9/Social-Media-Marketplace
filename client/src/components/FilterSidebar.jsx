57681c8c0beaa26edefe676ee141584f3e821d66
jahedripon9 <jahedahmed3170@gmail.com>
2026-05-26 12:16:58 +0600

    update filter

--- client/src/components/FilterSidebar.jsx
+++ client/src/components/FilterSidebar.jsx
@@ -1,7 +1,23 @@
 import { Filter, X } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 const FilterSidebar = ({ showFilterPhone, setShowFilterPhone, filters, setFilters }) => {
+
+    const navigate = useNavigate(); 
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [search, setSearch] = useState(searchParams.get("search") || "");
+
+    const onChangeSearch = (e) => {
+        if (e.target.value) {
+            setSearchParams({ search: e.target.value })
+            setSearch(e.target.value)
+        } else {
+            navigate(`/marketplace`)
+            setSearch("")
+        }
+    }
+
     return (
         <div className={`${showFilterPhone ? "max-sm:fixed" : "max-sm:hidden"} max-sm:inset-0 z-100 max-sm:h-screen max-sm:overflow-scroll bg-white rounded-lg shadow-sm border border-gray-200 h-fit sticky top-24 md:min-w-[300px]`}>
             <div className='p-4 border border-gray-200'>
@@ -14,6 +30,16 @@ const FilterSidebar = ({ showFilterPhone, setShowFilterPhone, filters, setFilter
                         <X className='size-6 text-gray-500 hover:text-gray-700 p-1 hover:bg-gray-100 rounded transition-colors cursor-pointer' />
                         <button onClick={()=>setShowFilterPhone(false)} className='sm:hidden text-sm border text-gray-700 px-3 py-1 rounded'>Apply</button>
                     </div>
+                </div>
+            </div>
+
+            <div className='p-4 space-y-6 sm:max-h-[calc(100vh-200px)] overflow-y-scroll no-scrollbar'>
+                {/* Search Bar*/}
+                <div className='flex items-center justify-between'>
+                    <input type="text" placeholder=" Search by username, platform, nich, etc." className=" w-full text-sm px3 py-2 border border-gray-300 rounded-md outline-indigo-500" onChange={onChangeSearch} value={search} />
+                    
+
+
                 </div>
             </div>
         </div>
