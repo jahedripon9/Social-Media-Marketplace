ff0e1dda3b3b69cc6ec462f056bc7247f78820ab
jahedripon9 <jahedahmed3170@gmail.com>
2026-09-13 00:06:39 +0600

    Initialize backend with Express, CORS, dotenv, Multer and Nodemon

--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => 
+app.get("/",
+    (req, res) => 
   res.send("Server is Live!"));
 
 const PORT = process.env.PORT || 3000;
