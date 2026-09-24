253e4c48c7c16ed81d08ea243b10fcd8c37ebff7
jahedripon9 <jahedahmed3170@gmail.com>
2026-09-25 00:03:10 +0600

    Inngest function to save user data to a database,Check if user already exists in the database, update user data if it exists

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,45 @@
 import { Inngest } from "inngest";
+import prisma from '../configs/prisma.js'
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "profile-marketplace" });
 
+// Inngest function to save user data to a database:
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk", triggers: [{ event: "clerk/user.created" }] },
+  async ({ event }) => {
+      const { data } = event
+      
+      //Check if user already exists in the database
+      const user = await prisma.user.findFirst({
+          where:{id:data.id}
+      })
+      if (user) {
+          //update user data if it exists
+          await prisma.user.update({
+              where: { id: data.id },
+              data:{
+                  email: data?.email_addresses[0]?.email_address,
+                  name: data?.first_name + " " + data?.last_name,
+                  image: data?.image_url,
+              }
+          })
+          return;
+      }
+      await prisma.user.create({
+          data: {
+                    id: data.id,
+                    email: data?.email_addresses[0]?.email_address,
+                    name: data?.first_name + " " + data?.last_name,
+                    image: data?.image_url,
+          }
+      })
+  },
+);
+//Inngest Function to delete
+
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [
+    syncUserCreation
+];
--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
 
 const app = express();
 
@@ -11,6 +14,7 @@ app.use(clerkMiddleware())
 
 app.get("/",(req, res) => 
   res.send("Server is Live!"));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 3000;
 
