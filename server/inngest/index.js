293aec3c612221ff90d4a28540af881c53ac9eb9
jahedripon9 <jahedahmed3170@gmail.com>
2026-09-25 21:14:09 +0600

    feat: add user update and deletion sync

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import prisma from '../configs/prisma.js'
+import prisma from "../configs/prisma.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "profile-marketplace" });
@@ -8,38 +8,89 @@ export const inngest = new Inngest({ id: "profile-marketplace" });
 const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk", triggers: [{ event: "clerk/user.created" }] },
   async ({ event }) => {
-      const { data } = event
-      
-      //Check if user already exists in the database
-      const user = await prisma.user.findFirst({
-          where:{id:data.id}
-      })
-      if (user) {
-          //update user data if it exists
-          await prisma.user.update({
-              where: { id: data.id },
-              data:{
-                  email: data?.email_addresses[0]?.email_address,
-                  name: data?.first_name + " " + data?.last_name,
-                  image: data?.image_url,
-              }
-          })
-          return;
-      }
-      await prisma.user.create({
-          data: {
-                    id: data.id,
-                    email: data?.email_addresses[0]?.email_address,
-                    name: data?.first_name + " " + data?.last_name,
-                    image: data?.image_url,
-          }
-      })
+    const { data } = event;
+
+    //Check if user already exists in the database
+    const user = await prisma.user.findFirst({
+      where: { id: data.id },
+    });
+    if (user) {
+      //update user data if it exists
+      await prisma.user.update({
+        where: { id: data.id },
+        data: {
+          email: data?.email_addresses[0]?.email_address,
+          name: data?.first_name + " " + data?.last_name,
+          image: data?.image_url,
+        },
+      });
+      return;
+    }
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data?.email_addresses[0]?.email_address,
+        name: data?.first_name + " " + data?.last_name,
+        image: data?.image_url,
+      },
+    });
   },
 );
-//Inngest Function to delete
+//Inngest Function to delete user from database
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-whit-clerk", triggers: [{ event: "clerk/user.deleted" }] },
+  async ({ event }) => {
+    const { data } = event;
 
+    const listing = await prisma.listing.findMany({
+      where: { ownerId: data.id },
+    });
+
+    const chat = await prisma.chat.findMany({
+      where: { OR: [{ ownerUserId: data.id }, { chatUserId: data.id }] },
+    });
+    const transactions = await prisma.transaction.findMany({
+      where: { userId: data.id },
+    });
+
+    if (
+      listing.length === 0 &&
+      chat.length === 0 &&
+      transactions.length === 0
+    ) {
+      await prisma.user.delete({ where: { id: data.id } });
+    } else {
+      await prisma.listing.updateMany({
+        where: { ownerId: data.id },
+        data: { status: "inactive" },
+      });
+    }
+  },
+);
+
+// Inngest function to Update user data in database:
+const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-from-clerk",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.user.update({
+      where: { id: data.id },
+      data: {
+        email: data?.email_addresses[0]?.email_address,
+        name: data?.first_name + " " + data?.last_name,
+        image: data?.image_url,
+      },
+    });
+  },
+);
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
-    syncUserCreation
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation
 ];
